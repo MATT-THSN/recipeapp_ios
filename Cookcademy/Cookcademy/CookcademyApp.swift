5c729c1d0a2fbaa0f10fbd71e3fecda2e94a3954
Matt Thompson <matt.thsn@icloud.com>
2022-07-10 11:49:40 +0200

    Initialise project files

---
+++ Cookcademy/Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Matthew Thompson on 10/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
