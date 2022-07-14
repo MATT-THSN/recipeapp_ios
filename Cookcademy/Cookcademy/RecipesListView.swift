fad0c00ad6124704247a9d91492fb53c22627921
Matt Thompson <matt.thsn@icloud.com>
2022-07-14 10:16:29 +0200

    Refactor code to change 'ContentView' to 'RecipiesListView'

--- Cookcademy/Cookcademy/RecipesListView.swift
+++ Cookcademy/Cookcademy/RecipesListView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RecipiesListView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct RecipiesListView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        RecipiesListView()
     }
 }
