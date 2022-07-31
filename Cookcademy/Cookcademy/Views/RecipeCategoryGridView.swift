dc21ccf68ed5e4a4883f98bc673e9b5f054bc30a
Matt Thompson <matt.thsn@icloud.com>
2022-07-31 19:02:45 +0100

    Add RecipeCategoryGridView file

---
+++ Cookcademy/Cookcademy/Views/RecipeCategoryGridView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeCategoryGridView.swift
+//  Cookcademy
+//
+//  Created by Matthew Thompson on 31/07/2022.
+//
+
+import SwiftUI
+
+struct RecipeCategoryGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeCategoryGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeCategoryGridView()
+    }
+}
