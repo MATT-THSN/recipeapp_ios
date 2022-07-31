77ae7c2bffc3226ae6d994b848f9d7bd18c2f76d
Matt Thompson <matt.thsn@icloud.com>
2022-07-31 21:37:54 +0100

    Refactor code to tidy up body

--- Cookcademy/Cookcademy/Views/RecipeCategoryGridView.swift
+++ Cookcademy/Cookcademy/Views/RecipeCategoryGridView.swift
@@ -12,22 +12,33 @@ struct RecipeCategoryGridView: View {
     private var recipeData = RecipeData()
     
     var body: some View {
+        let columns = [GridItem(), GridItem()]
+        
         NavigationView {
             ScrollView {
                 // Display each food category in two columns
-                LazyVGrid(columns: [GridItem(), GridItem()], content: {
+                LazyVGrid(columns: columns, content: {
                     ForEach(MainInformation.Category.allCases, id: \.self) { category in
-                        ZStack {
-                            Image(category.rawValue)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                            Text(category.rawValue)
-                                .font(.title)
-                        }
+                        CategoryView(category: category)
                     }
                 })
                 .navigationTitle("Categories")
             }
+        }
+    }
+}
+
+struct CategoryView: View {
+    let category: MainInformation.Category
+    
+    var body: some View {
+        ZStack {
+            Image(category.rawValue)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .opacity(0.45)
+            Text(category.rawValue)
+                .font(.title)
         }
     }
 }
