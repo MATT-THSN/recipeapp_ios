4c3ee1290064902a7710710a29c63783ea29911c
Matt Thompson <matt.thsn@icloud.com>
2022-08-17 15:51:34 +0100

    Build ModifyMainInformationView

--- Cookcademy/Cookcademy/Views/ModifyRecipes/ModifyMainInformationView.swift
+++ Cookcademy/Cookcademy/Views/ModifyRecipes/ModifyMainInformationView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct ModifyMainInformationView: View {
+    @Binding var mainInformation: MainInformation
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            TextField("Recipe Name", text: $mainInformation.name)
+            TextField("Author", text: $mainInformation.author)
+            Section(header: Text("Description")) {
+                TextEditor(text: $mainInformation.description)
+            }
+            Picker(selection: $mainInformation.category, label:
+                HStack {
+                    Text("Category")
+                    Spacer()
+                    Text(mainInformation.category.rawValue)
+                }, content: {
+                ForEach(MainInformation.Category.allCases, id: \.self) {
+                    Text($0.rawValue)
+                }
+            })
+            .pickerStyle(.menu)
+        }
     }
 }
 
 struct ModifyMainInformationView_Previews: PreviewProvider {
+    @State static var mainInformation = MainInformation(name: "Test Name",
+                                                            description: "Test Description",
+                                                            author: "Test Author",
+                                                            category: .breakfast)
+    
+    @State static var emptyInformation = MainInformation(name: "", description: "", author: "", category: .breakfast)
+
     static var previews: some View {
-        ModifyMainInformationView()
+        ModifyMainInformationView(mainInformation: $mainInformation)
+        ModifyMainInformationView(mainInformation: $emptyInformation)
     }
 }
